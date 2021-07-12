33a5688eb9b304d484047a7264b59aac1ebe4267
yasaniautami <85152207+yasaniautami@users.noreply.github.com>
2021-07-12 23:14:22 +0700

    Add files via upload

---
+++ app/src/main/java/com/yasania/appbiodata/helper/DataHelper.java
@@ -0,0 +1,35 @@
+package com.yasania.appbiodata.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by yasania
+ */
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('001', 'Yasania Utami', '2001-11-08', 'Perempuan','Pekanbaru');";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+
+    }
+
+}
